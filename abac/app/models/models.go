8525af5ec7e6133c7938379056479926e815d126
raisultan <ki.xbozz@gmail.com>
2021-04-17 00:00:39 +0700

    user model: raw

--- abac/app/models/models.go
+++ abac/app/models/models.go
@@ -1,6 +1,11 @@
 package models
 
-type User struct {
+import (
+	"database/sql"
+	"errors"
+)
+
+type user struct {
 	ID         int64  `json:"id"`
 	Email      string `json:"string"`
 	FirstName  string `json:"firstName"`
@@ -8,4 +13,20 @@ type User struct {
 	CreatedAt  string `json:"createdAt"`
 	IsAdmin    bool   `json:"isAdmin"`
 	IsApproved bool   `json:"isApproved"`
+}
+
+func (u *user) getUser(db *sql.DB) error {
+	return errors.New("Not implemented")
+}
+
+func (u *user) updateUser(db *sql.DB) error {
+	return errors.New("Not implemented")
+}
+
+func (u *user) deleteUser(db *sql.DB) error {
+	return errors.New("Not implemented")
+}
+
+func (u *user) createUser(db *sql.DB) error {
+	return errors.New("Not implemented")
 }
