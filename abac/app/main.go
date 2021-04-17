26f40ac3c8fc47aff43126c7456c7532eb94410a
raisultan <ki.xbozz@gmail.com>
2021-04-17 14:49:41 +0700

    updated

--- abac/app/main.go
+++ abac/app/main.go
@@ -2,14 +2,12 @@ package main
 
 import "os"
 
-const defaultPort = ":8080"
-
 func main() {
 	a := App{}
 	a.Initialize(
-		os.Getenv("DB_USERNAME"),
-		os.Getenv("DB_PASSWORD"),
-		os.Getenv("DB_NAME"),
+		os.Getenv("POSTGRES_USER"),
+		os.Getenv("POSTGRES_PASSWORD"),
+		os.Getenv("POSTGRES_DB"),
 	)
 
 	a.Run(defaultPort)
