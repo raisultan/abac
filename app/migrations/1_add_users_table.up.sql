36804f479f695b58e820810b8280a25b9484034e
raisultan <ki.xbozz@gmail.com>
2021-04-17 19:26:25 +0700

    unique constraint

--- app/migrations/1_add_users_table.up.sql
+++ app/migrations/1_add_users_table.up.sql
@@ -1,8 +1,8 @@
 CREATE TABLE IF NOT EXISTS users
 (
     id SERIAL,
-    email TEXT NOT NULL,
-    password TEXT NOT NULL,
+    email VARCHAR(256) NOT NULL UNIQUE,
+    password VARCHAR(256) NOT NULL,
     firstName TEXT NOT NULL,
     lastName TEXT NOT NULL,
     createdAt TEXT DEFAULT NULL,
