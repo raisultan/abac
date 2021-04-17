5d8695101b19ea1b654e14df4ffdaa6d7bc20ea7
raisultan <ki.xbozz@gmail.com>
2021-04-17 14:49:50 +0700

    products quires

--- abac/app/models.go
+++ abac/app/models.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"database/sql"
-	"errors"
 )
 
 type user struct {
@@ -15,22 +14,63 @@ type user struct {
 	IsApproved bool   `json:"isApproved"`
 }
 
-func (u *user) getUser(db *sql.DB) error {
-	return errors.New("Not implemented")
+type product struct {
+	ID    int     `json:"id"`
+	Name  string  `json:"name"`
+	Price float64 `json:"price"`
 }
 
-func (u *user) updateUser(db *sql.DB) error {
-	return errors.New("Not implemented")
+func (p *product) getProduct(db *sql.DB) error {
+	return db.QueryRow("SELECT name, price FROM products WHERE id=$1",
+		p.ID).Scan(&p.Name, &p.Price)
 }
 
-func (u *user) deleteUser(db *sql.DB) error {
-	return errors.New("Not implemented")
+func (p *product) updateProduct(db *sql.DB) error {
+	_, err :=
+		db.Exec("UPDATE products SET name=$1, price=$2 WHERE id=$3",
+			p.Name, p.Price, p.ID)
+
+	return err
 }
 
-func (u *user) createUser(db *sql.DB) error {
-	return errors.New("Not implemented")
+func (p *product) deleteProduct(db *sql.DB) error {
+	_, err := db.Exec("DELETE FROM products WHERE id=$1", p.ID)
+
+	return err
 }
 
-func (u *user) getUsers(db *sql.DB) error {
-	return errors.New("Not implemented")
+func (p *product) createProduct(db *sql.DB) error {
+	err := db.QueryRow(
+		"INSERT INTO products(name, price) VALUES($1, $2) RETURNING id",
+		p.Name, p.Price).Scan(&p.ID)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func getProducts(db *sql.DB, start, count int) ([]product, error) {
+	rows, err := db.Query(
+		"SELECT id, name,  price FROM products LIMIT $1 OFFSET $2",
+		count, start)
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	products := []product{}
+
+	for rows.Next() {
+		var p product
+		if err := rows.Scan(&p.ID, &p.Name, &p.Price); err != nil {
+			return nil, err
+		}
+		products = append(products, p)
+	}
+
+	return products, nil
 }
