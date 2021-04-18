297d5e3688991ae05f6d4d7807f16fd5c76724a5
raisultan <ki.xbozz@gmail.com>
2021-04-18 08:52:17 +0700

    delete added

---
+++ pkg/delete/service.go
@@ -0,0 +1,21 @@
+package retrieve
+
+type Service interface {
+	DeleteUser(int) error
+}
+
+type Repository interface {
+	DeleteUser(int) error
+}
+
+type service struct {
+	r Repository
+}
+
+func NewService(r Repository) Service {
+	return &service{r}
+}
+
+func (s *service) DeleteUser(id int) error {
+	return s.r.DeleteUser(id)
+}
